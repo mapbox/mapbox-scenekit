43cabdae6e73d661aa77cc85f467b6105d8bbf01
Avi Cieplinski <avi@mapbox.com>
2018-10-29 15:59:24 -0700

    Switch to barrier DispatchQueue

--- MapboxSceneKit/Tile Fetching/ImageBuilder.swift
+++ MapboxSceneKit/Tile Fetching/ImageBuilder.swift
@@ -2,10 +2,11 @@ import Foundation
 import UIKit
 
 internal final class ImageBuilder {
+    private let context: CGContext?
     private let clippedRect: CGRect
     private let tileSize: CGSize
     private let imageSize: CGSize
-    private var tileImages: [(Int, Int, UIImage)] = [(Int, Int, UIImage)]()
+    private let compositingDispatchQueue = DispatchQueue(label: "com.mapbox.SceneKit.compositing", attributes: .concurrent)
 
     init(xs: Int, ys: Int, tileSize: CGSize, insets: UIEdgeInsets) {
         self.imageSize = CGSize(width: CGFloat(xs) * tileSize.width, height: CGFloat(ys) * tileSize.height)
@@ -13,34 +14,33 @@ internal final class ImageBuilder {
                                height: imageSize.height - insets.top - insets.bottom)
         self.clippedRect = CGRect(x: insets.left, y: insets.top, width: finalSize.width, height: finalSize.height)
         self.tileSize = tileSize
-    }
-
-    func addTile(x: Int, y: Int, image: UIImage) {
-        tileImages.append((x, y, image))
-    }
-
-    func makeImage() -> UIImage? {
-
         let colorSpace: CGColorSpace = CGColorSpaceCreateDeviceRGB()
         let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
-        let context = CGContext(data: nil, width: Int(imageSize.width), height: Int(imageSize.height), bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)
+        context = CGContext(data: nil, width: Int(imageSize.width), height: Int(imageSize.height), bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)
 
         if context == nil {
             NSLog("Error creating CGContext")
         }
+    }
 
-        tileImages.forEach { (tileTuple) in
-            if let context = context, let cgImage = tileTuple.2.cgImage {
-                context.draw(cgImage, in: CGRect(origin: CGPoint(x: CGFloat(tileTuple.0) * tileSize.width, y: CGFloat(Int(imageSize.height / tileSize.height) - tileTuple.1 - 1) * tileSize.height), size: tileSize))
+    func addTile(x: Int, y: Int, image: UIImage) {
+        if let context = context, let cgImage = image.cgImage {
+            compositingDispatchQueue.sync(flags: .barrier) { [weak self] in
+                guard let self = self else { return }
+
+                context.draw(cgImage, in: CGRect(origin: CGPoint(x: CGFloat(x) * self.tileSize.width, y: CGFloat(Int(self.imageSize.height / self.tileSize.height) - y - 1) * self.tileSize.height), size: self.tileSize))
             }
+        } else if image.cgImage == nil {
+            print("cgimage is nil")
+        } else if context == nil {
+            print("context is nil")
         }
+    }
 
-        tileImages.removeAll()
-
+    func makeImage() -> UIImage? {
         guard let fullImageRef = context?.makeImage(), let croppedImageRef = fullImageRef.cropping(to: clippedRect) else {
             return nil
         }
-
         return UIImage(cgImage: croppedImageRef)
     }
 }
