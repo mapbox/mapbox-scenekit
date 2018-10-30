b5ba210ea527bc0e38d674d406410ee79c78609d
Avi Cieplinski <avi@mapbox.com>
2018-10-30 16:48:29 -0700

    lower texture image size as a text.

--- MapboxSceneKit/StaticConstants.swift
+++ MapboxSceneKit/StaticConstants.swift
@@ -13,7 +13,7 @@ struct Constants {
     static let maxTerrainRGBZoomLevel: Int = 12
     
     /// Is the largest supported texture size by the SDK, style images are drawn at 2x this value, so any higher number would result in crashes.
-    static let maxTextureImageSize: Int = 1024
+    static let maxTextureImageSize: Int = 512
     static let maxTextureSubdivisionFactor: Int = 3
     
     /// The maximum number of terrain generation attempts to make before terrainNode generation is cancelled, returning an error.
